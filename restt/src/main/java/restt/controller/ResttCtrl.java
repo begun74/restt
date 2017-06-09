0da877cb3428cb5eb1ca158022944ee59e26dc8d
vbegun <vbegun@IT-AZ.interfarmax.local>
2017-06-09 15:32:18 +0300

    1

--- restt/src/main/java/restt/controller/ResttCtrl.java
+++ restt/src/main/java/restt/controller/ResttCtrl.java
@@ -7,11 +7,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import restt.model.User;
 
 
-
+@RestController
 public class ResttCtrl {
 	
     @RequestMapping(value = "/user/", method = RequestMethod.GET)
